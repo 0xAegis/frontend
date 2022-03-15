5dc55d0ca4630d27d5a2f182ceaded89a1120017
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-15 18:04:22 +0530

    feat: add Navigation (top and side navbar) (#6)
    Co-authored-by: Sumit Ghosh <sumit@sumit-ghosh.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Group } from "@mantine/core";
 
+import Navigation from "./components/navigation/Navigation";
 import ConnectWallet from "./components/connect-wallet/ConnectWallet";
 import ConnectArcana from "./components/arcana/connect-arcana/ConnectArcana";
 
 const App = () => {
   return (
-    <>
+    <Navigation>
       <Group direction="column">
         <Group>
           <ConnectWallet />
@@ -14,7 +15,7 @@ const App = () => {
           <ConnectArcana />
         </Group>
       </Group>
-    </>
+    </Navigation>
   );
 };
 
