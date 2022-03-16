5b1a3ca83fc17b587f3d40adf16e4c02dbbe967e
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-03-16 20:48:15 +0530

    Add user profile pages

--- src/features/user-profile/UserProfile.js
+++ src/features/user-profile/UserProfile.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+
 import { useParams } from "react-router-dom";
+import { Text } from "@mantine/core";
+import { ethers } from "ethers";
+
+import { getUser } from "../../utils/aegis";
 
 export const UserProfile = () => {
-  let params = useParams();
-  return "user profile of: " + params.username;
+  const [userInfo, setUserInfo] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const userInfo = await getUser({ provider, account: params.username });
+      setUserInfo(userInfo);
+    };
+
+    fetchUserInfo();
+  }, [params.username]);
+
+  return userInfo ? <Text>{userInfo.username}</Text> : <Text>not found</Text>;
 };
