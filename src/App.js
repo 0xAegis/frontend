de7771e5b406f4a636a2e0e31c9885258e1c7c54
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-17 22:17:08 +0530

    feat(ui): place connect buttons on side navbar (#20)
    * feat: place connect buttons on side navbar
    
    * make connect buttons span full width

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Group } from "@mantine/core";
 import { Outlet } from "react-router-dom";
 
-import ConnectArcana from "./features/auth/connect-arcana/ConnectArcana";
-import ConnectWallet from "./features/auth/connect-wallet/ConnectWallet";
 import { CreateUser } from "./features/auth/create-user/CreateUser";
 import Navigation from "./features/navigation/Navigation";
 import { CreatePost } from "./features/posts/create-post/CreatePost";
@@ -12,13 +10,6 @@ const App = () => {
     <Navigation>
       <Group direction="column">
         <Group>
-          <ConnectWallet />
-        </Group>
-        <Group>
-          <ConnectArcana />
-        </Group>
-        <Group>
-          <CreatePost />
           <CreateUser />
         </Group>
       </Group>
