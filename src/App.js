59fdc80cd120560773b1ee4a7e44ae1b596e6c73
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-17 20:15:56 +0530

    feat: place connect buttons on side navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Group } from "@mantine/core";
 import { Outlet } from "react-router-dom";
 
-import ConnectArcana from "./features/auth/connect-arcana/ConnectArcana";
-import ConnectWallet from "./features/auth/connect-wallet/ConnectWallet";
 import { CreateUser } from "./features/auth/create-user/CreateUser";
 import Navigation from "./features/navigation/Navigation";
 
@@ -10,12 +8,6 @@ const App = () => {
   return (
     <Navigation>
       <Group direction="column">
-        <Group>
-          <ConnectWallet />
-        </Group>
-        <Group>
-          <ConnectArcana />
-        </Group>
         <Group>
           <CreateUser />
         </Group>
