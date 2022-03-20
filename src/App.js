f77f5b0c0fd871f53be3147308d2aee670295f8b
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-20 20:01:09 +0530

    use container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useContext } from "react";
 
-import { Group } from "@mantine/core";
+import { Group, Container } from "@mantine/core";
 import { observer } from "mobx-react-lite";
 import { Outlet } from "react-router-dom";
 import { ethers } from "ethers";
@@ -74,11 +74,13 @@ const App = observer(() => {
 
   return (
     <Navigation>
-      <Group direction="column">
-        <CreatePost />
-        <CreateUser />
-      </Group>
-      <Outlet />
+      <Container>
+        <Group direction="column">
+          <CreatePost />
+          <CreateUser />
+        </Group>
+        <Outlet />
+      </Container>
     </Navigation>
   );
 });
