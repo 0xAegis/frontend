61db93f79982c26e1d40fcbc8e3a42f5cfe1c2e3
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-20 19:59:08 +0530

    display post as a card with paid badge

--- src/features/posts/post/Post.js
+++ src/features/posts/post/Post.js
@@ -1,5 +1,24 @@
-import { Text } from "@mantine/core";
+import { Card, Text, Badge, Group, useMantineTheme } from "@mantine/core";
 
 export const Post = ({ text, attachments, isPaid }) => {
-  return <Text>{text}</Text>;
+  // return <Text>{text}</Text>;
+  const theme = useMantineTheme();
+
+  return (
+    <div style={{ width: 500, margin: "auto" }}>
+      <Card shadow="sm" p="lg">
+        <Text>{text}</Text>
+        <Group
+          position="right"
+          style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+        >
+          {isPaid ? (
+            <Badge color="blue" variant="light">
+              Paid
+            </Badge>
+          ) : null}
+        </Group>
+      </Card>
+    </div>
+  );
 };
