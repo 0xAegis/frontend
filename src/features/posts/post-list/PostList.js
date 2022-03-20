e46b3edd9452b64d38d6f4367706c1e75b5271b7
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-20 20:00:23 +0530

    display all posts as a table

--- src/features/posts/post-list/PostList.js
+++ src/features/posts/post-list/PostList.js
@@ -1,12 +1,28 @@
-import { Group } from "@mantine/core";
+import { Group, Table } from "@mantine/core";
 import { Post } from "../post/Post";
-
+import "./postList.css";
 export const PostList = ({ posts }) => {
+  const postRows = posts.map((post, index) => (
+    <tr key={index}>
+      <td>
+        <Post {...post} key={post.user + "::" + post.postIndex.toString()} />
+      </td>
+    </tr>
+  ));
+
   return (
     <Group direction="column">
-      {posts.map((post) => (
+      {/* {posts.map((post) => (
         <Post {...post} key={post.user + "::" + post.postIndex.toString()} />
-      ))}
+      ))} */}
+      <Table>
+        <thead>
+          <tr>
+            <th>Posts</th>
+          </tr>
+        </thead>
+        <tbody>{postRows}</tbody>
+      </Table>
     </Group>
   );
 };
