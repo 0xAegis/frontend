6383b79490f4d7ac804b73f069c7f5837adbcd6a
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-23 00:32:44 +0530

    feat: add download attachments button on posts - download attachments button downloads all the files attached to the post

--- src/features/posts/post/Post.js
+++ src/features/posts/post/Post.js
@@ -1,15 +1,44 @@
-import { Card, Text, Badge, Group, useMantineTheme } from "@mantine/core";
+import { useContext } from "react";
 
+import {
+  Card,
+  Text,
+  Badge,
+  Group,
+  useMantineTheme,
+  Button,
+} from "@mantine/core";
+
+import { getArcanaStorage, downloadFromArcana } from "../../../utils/arcana";
+import { AppContext } from "../../..";
 export const Post = ({ text, attachments, isPaid }) => {
+  const appStore = useContext(AppContext);
   // return <Text>{text}</Text>;
   const theme = useMantineTheme();
+
+  const handleAttachmentDownload = async () => {
+    // Download the files from Arcana
+    const arcanaStorage = getArcanaStorage({
+      privateKey: appStore.arcanaAccount.privateKey,
+      email: appStore.arcanaAccount.userInfo.email,
+    });
+    await Promise.all(
+      attachments.map(
+        async (fileDid) =>
+          await downloadFromArcana({
+            arcanaStorage,
+            fileDid: fileDid,
+          })
+      )
+    );
+  };
 
   return (
     <div style={{ width: 500, margin: "auto" }}>
       <Card shadow="sm" p="lg">
         <Text>{text}</Text>
         <Group
-          position="right"
+          position="apart"
           style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
         >
           {isPaid ? (
@@ -17,6 +46,9 @@ export const Post = ({ text, attachments, isPaid }) => {
               Paid
             </Badge>
           ) : null}
+          <Button onClick={handleAttachmentDownload}>
+            Download Attachments
+          </Button>
         </Group>
       </Card>
     </div>
