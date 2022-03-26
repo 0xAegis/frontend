61e0a01ce5edd8ed1daab147157c1abc1d9c7784
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-27 03:51:31 +0530

    fix: add missing NotificationsProvider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { createContext } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NotificationsProvider } from "@mantine/notifications";
 
 import App from "./App";
 import { GoogleOauthRedirect } from "./routes/oauth";
@@ -18,22 +19,24 @@ const appStore = new AppStore();
 ReactDOM.render(
   <React.StrictMode>
     <AppContext.Provider value={appStore}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<App />}>
-            <Route path="/create-account" element={<CreateUser />} />
-            <Route path="user">
-              <Route path=":userPubKey/followed" element={<FollowedPage />} />
-              <Route path=":userPubKey" element={<UserProfile />} />
+      <NotificationsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<App />}>
+              <Route path="/create-account" element={<CreateUser />} />
+              <Route path="user">
+                <Route path=":userPubKey/followed" element={<FollowedPage />} />
+                <Route path=":userPubKey" element={<UserProfile />} />
+              </Route>
+              <Route path="/" element={<Home />}></Route>
             </Route>
-            <Route path="/" element={<Home />}></Route>
-          </Route>
-          <Route
-            path="/oauth-redirect/google"
-            element={<GoogleOauthRedirect />}
-          />
-        </Routes>
-      </BrowserRouter>
+            <Route
+              path="/oauth-redirect/google"
+              element={<GoogleOauthRedirect />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </NotificationsProvider>
     </AppContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
