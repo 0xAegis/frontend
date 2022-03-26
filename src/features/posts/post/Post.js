68a98bedfb35ced8a52cf2622784b3f8f198daa4
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2022-03-27 03:21:48 +0530

    feat: post include name of user, user profile link

--- src/features/posts/post/Post.js
+++ src/features/posts/post/Post.js
@@ -1,7 +1,37 @@
-import { Card, Text, Badge, Group, useMantineTheme } from "@mantine/core";
+import { useEffect, useContext, useState } from "react";
+import { ethers } from "ethers";
+import { Link } from "react-router-dom";
+import {
+  Card,
+  Text,
+  Badge,
+  Group,
+  useMantineTheme,
+  Button,
+} from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
+import { AppContext } from "../../..";
+import { getUser } from "../../../utils/aegis";
 
-export const Post = ({ text, attachments, isPaid }) => {
+export const Post = ({ user, text, attachments, isPaid }) => {
+  const appStore = useContext(AppContext);
+  const [userName, setUserName] = useState(null);
+  useEffect(() => {
+    const getUserNames = async () => {
+      if (!window.ethereum) {
+        console.log("Metamask is not installed.");
+        return;
+      }
+      //Checking If connection status is false
+      if (!appStore.connectionStatus) {
+        return;
+      }
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const fetchedUser = await getUser({ provider, account: user });
+      setUserName(fetchedUser.name);
+    };
+    getUserNames();
+  }, [appStore.connectionStatus, user]);
   const theme = useMantineTheme();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
@@ -9,10 +39,14 @@ export const Post = ({ text, attachments, isPaid }) => {
     <div style={isMobile ? { width: "70vw" } : { width: 500 }}>
       <Card shadow="sm" p="lg">
         <Text>{text}</Text>
+
         <Group
           position="right"
           style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
         >
+          <Link to={"/user/" + user}>
+            <Button>{userName}</Button>
+          </Link>
           {isPaid ? (
             <Badge color="blue" variant="light">
               Paid
